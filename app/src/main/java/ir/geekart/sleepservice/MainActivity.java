d1fadad04c9d3b956feff033ebaa560327e25445
SajjadSaraji <Sajjad.Saraji@gmail.com>
2015-12-15 13:40:23 +0330

    Service And Countdown Commit

--- app/src/main/java/ir/geekart/sleepservice/MainActivity.java
+++ app/src/main/java/ir/geekart/sleepservice/MainActivity.java
@@ -10,27 +10,47 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity {
     Button startButton, stopButton;
-    TextView Status_true, Status_false;
+    TextView Status_true, Status_false,countdown_txt;
     static Context context;
     boolean isPlaying;
     Intent streamService;
     SharedPreferences prefs;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        Casting View Items
+         */
+        streamService = new Intent(MainActivity.this, ExampleService.class);
         context = this;
         startButton = (Button) findViewById(R.id.startButton);
         stopButton = (Button) findViewById(R.id.stopButton);
         Status_true = (TextView)findViewById(R.id.Service_Status_true);
         Status_false = (TextView)findViewById(R.id.Service_Status_false);
-
+        countdown_txt = (TextView)findViewById(R.id.countdown_txt);
         prefs = PreferenceManager.getDefaultSharedPreferences(context);
         getPrefs();
-        streamService = new Intent(MainActivity.this, StreamService.class);
+
+
+
+        /*
+        Declare Countdown
+         */
+        long time = 10000;
+
+        final TimerForService TFS = new TimerForService(getApplicationContext(),countdown_txt
+                ,time,streamService);
+
+
+
 
         startButton.setOnClickListener(new View.OnClickListener() {
 
@@ -39,6 +59,8 @@ public class MainActivity extends AppCompatActivity {
                 // TODO Auto-generated method stub
                 startService(streamService);
                 startButton.setEnabled(false);
+                long time = 30000;
+                TFS.Start();
                 Status_true.setText("True");
                 Status_false.setText("");
             }
@@ -53,6 +75,7 @@ public class MainActivity extends AppCompatActivity {
                 startButton.setEnabled(true);
                 Status_true.setText("");
                 Status_false.setText("False");
+                TFS.Stop();
             }
         });
     }
